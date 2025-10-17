16b75515ca381ec2769666ba65a0979679367eef
utahphotohunter <mitch.hunter1197@gmail.com>
2025-10-16 20:02:27 -0600

    started working on the views for inventory classification

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,9 +1,9 @@
-const utilities = require("../utilities/")
-const baseController = {}
+const utilities = require("../utilities/");
+const baseController = {};
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
-}
+baseController.buildHome = async function (req, res) {
+  const nav = await utilities.getNav();
+  res.render("index", { title: "Home", nav });
+};
 
-module.exports = baseController
+module.exports = baseController;
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,10 +1,21 @@
-const pool = require("../database/")
+const pool = require("../database/");
 
 /* ***************************
  *  Get all classification data
  * ************************** */
-async function getClassifications(){
-  return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
+async function getClassifications() {
+  return await pool.query(
+    "SELECT * FROM public.classification ORDER BY classification_name"
+  );
 }
 
-module.exports = {getClassifications}
+async function getClassificationById(id) {
+  return await pool.query(
+    `SELECT * FROM public.classification WHERE classification_id = ${id}`
+  );
+}
+
+module.exports = { 
+  getClassifications,
+  getClassificationById
+};
