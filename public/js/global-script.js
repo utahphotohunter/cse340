299559efe36c473f3035a8cd0ea76305fc4f0bcb
utahphotohunter <mitch.hunter1197@gmail.com>
2025-10-23 11:59:49 -0600

    finished detail page and modified responsiveness

--- public/js/global-script.js
+++ public/js/global-script.js
@@ -2,11 +2,19 @@ let url = window.location.href;
 let path = window.location.pathname;
 let type;
 let target;
+let identifier;
 if (url.includes("/inv/type/")) {
   type = url[url.length - 1];
-  target = document.getElementById(`nav-${type}`);
+  identifier = `nav-${type}`;
+  target = document.getElementById(identifier);
+  localStorage.setItem("wayfinder", identifier);
 } else if (path === "/" || path === "") {
-  target = document.getElementById("nav-0");
+  identifier = "nav-0";
+  target = document.getElementById(identifier);
+  localStorage.setItem("wayfinder", identifier);
+} else {
+  identifier = localStorage.getItem("wayfinder");
+  target = document.getElementById(identifier);
 }
 
 target.classList.add("current");
