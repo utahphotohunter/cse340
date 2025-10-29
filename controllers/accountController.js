3defcaf5dedcfe6614753e1e1b6efaa756414fb5
utahphotohunter <mitch.hunter1197@gmail.com>
2025-10-28 23:53:53 -0600

    added favicon, enabled flash messages, got naked login page done

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,15 +1,16 @@
-const utilities = require("../utilities/index");
+const utilities = require("../utilities/");
+const accountController = {};
 
 /* ****************************************
  *  Deliver login view
  * *************************************** */
-async function buildLogin(req, res, next) {
+accountController.buildLogin = async function (req, res, next) {
   let nav = await utilities.getNav();
-  req.flash("notice", flashMessage);
-  res.render("account/login", {
+  // req.flash("notice", flashMessage);
+  res.render("./account/login", {
     title: "Login",
     nav,
   });
-}
+};
 
-module.exports = { buildLogin };
+module.exports = accountController;
