8b47cafb72708326660e0ff9b924c17908d5b9c6
utahphotohunter <mitch.hunter1197@gmail.com>
2025-10-28 22:21:41 -0600

    added account controller and route. updated inv route to have error handler

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = new express.Router();
 const accountController = require("../controllers/accountController");
+const utilities = require("../utilities");
 
 // Route to build login page
-router.get("/login", accountController);
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 module.exports = router;
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
+const utilities = require("../utilities");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get(
+  "/type/:classificationId",
+  utilities.handleErrors(invController.buildByClassificationId)
+);
 
 // Route to build inventory detail view
-router.get("/detail/:inv_id", invController.buildByInvId);
+router.get(
+  "/detail/:inv_id",
+  utilities.handleErrors(invController.buildByInvId)
+);
 
 module.exports = router;
