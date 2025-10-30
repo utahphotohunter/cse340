f42ab97fcfe5f9419349785734010c341c6dc570
utahphotohunter <mitch.hunter1197@gmail.com>
2025-10-30 12:03:14 -0600

    started work on register form processing

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,9 +5,15 @@ const utilities = require("../utilities");
 
 // Route to build login page
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
+// Route to build registration page
 router.get(
   "/register",
   utilities.handleErrors(accountController.buildRegistration)
+);
+// POST route for registration page
+router.post(
+  "/register",
+  utilities.handleErrors(accountController.registerAccount)
 );
 
 module.exports = router;
